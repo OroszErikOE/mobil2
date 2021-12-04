d40f81248f692c2b429f244bd6559ca45b48f100
OroszErikOE <riki199925@gmail.com>
2021-12-04 09:22:43 +0100

    layout

--- app/src/main/java/com/example/projekt2/MainActivity.kt
+++ app/src/main/java/com/example/projekt2/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.projekt2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.example.projekt2.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityMainBinding
+    var baseCurrency = "HUF"
+    var convertedToCurrency = "USD"
+    var conversionRate = 0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
 }
